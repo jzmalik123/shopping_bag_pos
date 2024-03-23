60c093d9fb08335eba70ae89d11fab9c52690a05
Jahanzaib Malik <jzmalik123@gmail.com>
2024-03-23 16:57:05 +0500

    Add bag category to order

--- db/migrate/20240323114027_create_orders.rb
+++ db/migrate/20240323114027_create_orders.rb
@@ -2,6 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
       t.references :customer, foreign_key: true, index: true
+      t.references :bag_category, foreign_key: :true
       t.integer :payment_method
       t.date :order_date
       t.integer :total_amount
