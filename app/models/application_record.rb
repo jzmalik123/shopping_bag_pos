43229d9766d568b318d56a7b81e389ab7577bba8
Jahanzaib Malik <jzmalik123@gmail.com>
2024-03-23 12:58:13 +0500

    Install active admin

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.ransackable_attributes(_auth_object = nil)
+    attribute_names
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    reflections.keys
+  end
+  
 end
