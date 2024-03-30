1741e490d534c14765bb061a3797fa5f180ecae9
Jahanzaib Malik <jzmalik123@gmail.com>
2024-03-30 18:53:11 +0500

    Fix order items migration

--- db/migrate/20240323115440_create_order_items.rb
+++ db/migrate/20240323115440_create_order_items.rb
@@ -1,7 +1,7 @@
 class CreateOrderItems < ActiveRecord::Migration[7.1]
   def change
     create_table :order_items do |t|
-      t.references :order, foreign_key: :true, index: true
+      t.references :order, foreign_key: { on_delete: :cascade }, index: true
       t.references :bag_size, foreign_key: :true
       t.integer :rate
       t.integer :weight
