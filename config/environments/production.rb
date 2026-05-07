a3a97148c099bc07e5178ba7c4dbdede8e505584
jzmalik123 <jzmalik123@gmail.com>
2026-05-07 20:07:21 +0500

    update logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,10 +51,10 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
-  # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  # Log to file in production so Passenger/Nginx setups can tail Rails logs directly.
+  config.logger = ActiveSupport::Logger.new("log/production.log", 10, 50.megabytes)
+  config.logger.formatter = ::Logger::Formatter.new
+  config.logger = ActiveSupport::TaggedLogging.new(config.logger)
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -62,7 +62,7 @@ Rails.application.configure do
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
   # want to log everything, set the level to "debug".
-  config.log_level = "info"
+  config.log_level = :info
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
